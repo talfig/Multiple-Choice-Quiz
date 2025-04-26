6bfd03dde7141cd886a3ae2c65ca53f2fcb483a8
talfig <talfig8@gmail.com>
2025-04-26 17:17:02 +0300

    Creating logic and graphic classes for project

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,12 +5,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Quiz extends Application {
-    private static final int QUIZ_CANVAS_WIDTH_SIZE = 600;
+    private static final int QUIZ_WIDTH_SIZE = 600;
     private static final int QUIZ_HEIGHT_SIZE = 800;
 
     public void start(Stage stage) throws Exception{
         Parent root = (Parent) FXMLLoader.load(getClass().getResource("quiz.fxml"));
-        Scene scene = new Scene(root, QUIZ_CANVAS_WIDTH_SIZE, QUIZ_HEIGHT_SIZE);
+        Scene scene = new Scene(root, QUIZ_WIDTH_SIZE, QUIZ_HEIGHT_SIZE);
         stage.setTitle("Quiz");
         stage.setScene(scene);
         stage.show();
